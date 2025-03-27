fdefbad60124e3b0a6322888e1144e04684261c7
snelusha <hello@snelusha.dev>
2025-03-28 00:10:05 +0530

    chore: init repo

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import "@/styles/globals.css";
+
+import { geistMono, geistSans } from "@/styles/fonts";
+
+import { cn } from "@/styles/utils";
+
+import type { Metadata, Viewport } from "next";
+
+export const metadata: Metadata = {
+  title: "noise",
+  description: "Made by a human on earth!",
+  creator: "@snelusha",
+};
+
+export const viewport: Viewport = {
+  colorScheme: "only light",
+  width: "device-width",
+  maximumScale: 1,
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={cn(geistSans.variable, geistMono.variable)}>
+        {children}
+      </body>
+    </html>
+  );
+}
